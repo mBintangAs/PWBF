05cedcea3fc40d829af0a603211878a2d84652ac
Shabrina-Ailyndra <113023359+Shabrina-Ailyndra@users.noreply.github.com>
2022-10-03 16:29:14 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('No_Telepon');
+            $table->boolean('JK');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
